9d900700d004863764264afce883fce9b910966d
vahidalizadeh <vahid.alizadeh@depaul.edu>
2025-11-06 18:49:12 -0600

    assignment 4 problem

---
+++ src/main/java/assignment4/problem/App.java
@@ -0,0 +1,19 @@
+/*
+ *
+ *  * Copyright (c) 2023.
+ *  * Vahid Alizadeh
+ *  * Object-oriented Software Development
+ *  * DePaul University
+ *
+ */
+
+package assignment4.problem;
+
+public class App {
+
+	public static void main(String[] args) {
+		ConverterGUI ui = new ConverterGUI();
+
+	}
+
+}
---
+++ src/main/java/assignment4/problem/Client.java
@@ -0,0 +1,20 @@
+/*
+ *
+ *  * Copyright (c) 2023.
+ *  * Vahid Alizadeh
+ *  * Object-oriented Software Development
+ *  * DePaul University
+ *
+ */
+
+package assignment4.problem;
+
+public class Client {
+
+	//TODO Here is where you implement the converter logic using the requested "patterns" (Chain of Responsibility, and Decorator)
+	//TODO You can have a static method Convert(..) here
+	//TODO static Convert(...) method?
+	//TODO In the method body you must setup your patterns like Instantiating Handlers and setSuccessors for the Chain of Responsibility
+	//TODO and Decorators for the Decorator pattern
+
+}
